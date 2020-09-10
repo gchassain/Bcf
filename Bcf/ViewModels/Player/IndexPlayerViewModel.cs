8a12310315be9348106a144456313cfe3c6eacca
rahzou <62594607+gchassain@users.noreply.github.com>
2020-09-10 17:13:10 +0200

    Modif Players en Player

---
+++ Bcf/ViewModels/Player/IndexPlayerViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static Bcf.Models.Enums;
+
+namespace Bcf.ViewModels
+{
+    public class IndexPlayerViewModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Joueur")]
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public int Number { get; set; }
+        [Display(Name = "Poste")]
+        public PlayerPositionsEnum Position { get; set; }
+        [Display(Name = "Taille")]
+        public double Height { get; set; }
+        [Display(Name = "Poids")]
+        [DisplayFormat(DataFormatString = "{0:0.} kg")]
+        public double Weight { get; set; }
+        public string ProfilePicture { get; set; }
+    }
+}
